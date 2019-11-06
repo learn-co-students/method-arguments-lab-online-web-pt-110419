dc5cc2f188ac2e7562084aec16e24b5f1da6684a
Romario Gustave <romagustave10@gmail.com>
2019-11-06 06:32:07 +0000

    Done.

---
+++ lib/introduction.rb
@@ -0,0 +1,10 @@
+def introduction(name)
+  name = "Josh"
+  puts "Hi, my name is "+ name + "."
+end
+
+def introduction(name, language)
+  name = "Dan"
+  language = "ember.js"
+   puts "Hi, my name is "+ name + " and I am  learning to program in" + language"
+ end
