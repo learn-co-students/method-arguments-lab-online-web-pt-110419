35759e0e830e46d1819d686fe5cd47743a70da29
Mintra Phophaijit <bluembc@gmail.com>
2019-11-07 17:30:22 +0000

    Done.

--- spec/introduction_spec.rb
+++ spec/introduction_spec.rb
@@ -1,18 +1,8 @@
-
+  
 def introduction(name)
-  puts "Hi, my name is #{name}"
-end
-
-introduction("Josh")
-
-describe "#introduction_with_language" do 
-  it "takes in two arguments, a name and a language, and puts out a phrase using those arguments." do 
-    expect{introduction_with_language("Dan", "Ember.js")}.to output("Hi, my name is Dan and I am learning to program in Ember.js.\n").to_stdout
-  end
+  puts "Hi, my name is #{name}."
 end
 
 def introduction_with_language(name, language)
-  puts "Hi, my name is #{name}, and I am learning to program in #{langauge}"
+  puts "Hi, my name is #{name} and I am learning to program in #{language}.\n"
 end
-
-introduction_with_language("Ember.js")
